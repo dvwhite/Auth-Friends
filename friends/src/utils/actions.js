5c5cdaf37f29219ca0cacf119aeccd9508938331
David White <dwhite0627@gmail.com>
2020-03-13 22:30:42 -0400

    Refactor login to use async/await, add export statements for each function

--- friends/src/utils/actions.js
+++ friends/src/utils/actions.js
@@ -1,19 +1,20 @@
-import axios from 'axios';
-import axiosWithAuth from './axiosWithAuth';
+import axios from "axios";
+import axiosWithAuth from "./axiosWithAuth";
 
 /**
- * @function: Logs you into the API endpoint and obtains a JWT for server authentication
+ * @function: Sends a post to log you in and obtain an authentication token for the endpoint
  * @param {*} credentials: The credentials used to authenticate
- * @returns: none
+ * @returns {Promise} promise: The API promise to be resolved in useEffect or componentDidMount
  */
-const login = (credentials) => {
-  return axios.post('/api/login', credentials)
+export const login = async credentials => {
+  const promise = await axios.post("/api/login", credentials);
+  return promise
     .then(res => {
-      localStorage.setItem('token', res.data.payload);
+      localStorage.setItem("token", res.data.payload);
       return res;
     })
-    .catch(err => console.log("Error in login:", err.response))
-}
+    .catch(err => console.log("Error in actions>login:", err.response));
+};
 
 // Protected endpoints requiring an authentication token
 /**
@@ -21,69 +22,74 @@ const login = (credentials) => {
  * @param: none
  * @returns: none
  */
-const getAllFriends = () => {
-  axiosWithAuth().get('/api/friends')
+export const getAllFriends = () => {
+  axiosWithAuth()
+    .get("/api/friends")
     .then(res => {
-      console.log(res)
+      console.log(res);
       return res.data;
     })
     .catch(err => console.error(err.response));
-}
+};
 
 /**
  * @function: Send a GET req to /api/friends/123 to get the friend with that id from the API
  * @param {*} id: The id of the friend to update
- * @returns {*} The friend with the id passed in the req url 
+ * @returns {*} The friend with the id passed in the req url
  */
-const getFriend = (id) => {
-  axiosWithAuth().get(`/api/friends/${id}`)
+export const getFriend = id => {
+  axiosWithAuth()
+    .get(`/api/friends/${id}`)
     .then(res => {
-      console.log(res)
+      console.log(res);
       return res.data;
     })
     .catch(err => console.error(err.response));
-}
+};
 
 /**
- * @function: Send a POST req to /api/friends to create a friend in the API endpoint 
+ * @function: Send a POST req to /api/friends to create a friend in the API endpoint
  *            Note: Pass the friend as the body of the request (the second argument passed to axios.post).
- * @param {*} friend 
+ * @param {*} friend
  * @returns: The new list of friends
  */
-const addFriend = (friend) => {
-  axiosWithAuth().post('/api/friends', friend)
+export const addFriend = friend => {
+  axiosWithAuth()
+    .post("/api/friends", friend)
     .then(res => {
-      console.log(res)
+      console.log(res);
       return res.data;
     })
     .catch(err => console.error(err.response));
-}
+};
 
 /**
- * @function: Send a PUT req to /api/friends/:id to update the friend using the id passed as part of the URL. 
+ * @function: Send a PUT req to /api/friends/:id to update the friend using the id passed as part of the URL.
  *            Note: The friend param should be an object with the updated information
- * @param {*} friend 
+ * @param {*} friend
  * @returns: none (?)
  */
-const updateFriend = (friend) => {
-  axiosWithAuth().put(`/api/friends/:${friend.id}`, friend)
+export const updateFriend = friend => {
+  axiosWithAuth()
+    .put(`/api/friends/:${friend.id}`, friend)
     .then(res => {
-      console.log(res)
+      console.log(res);
       return res.data;
     })
     .catch(err => console.error(err.response));
-}
+};
 
 /**
  * @function: Send a DELETE req to /api/friends/${id} to remove the friend with that id from the endpoint
- * @param {*} id 
+ * @param {*} id
  * @returns: none (?)
  */
-const deleteFriend = (id) => {
-  axiosWithAuth().delete(`/api/friends/${id}`)
+export const deleteFriend = id => {
+  axiosWithAuth()
+    .delete(`/api/friends/${id}`)
     .then(res => {
-      console.log(res)
+      console.log(res);
       return res.data;
     })
     .catch(err => console.error(err.response));
-}
+};
